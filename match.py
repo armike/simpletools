c9167cbc836674efbef5f33b39da42002d409e6f
Mike Ross <armike@gmail.com>
2015-09-22 04:14:05 -0700

    Dial down crazy

--- match.py
+++ match.py
@@ -8,11 +8,11 @@ class Matcher(object):
     MATCH_QUALITY_HIGH = 100
     MATCH_QUALITY_WAY_HIGHER = 200
     MATCH_SPEED_FAST = 10
-    MATCH_SPEED_CRAZY_FAST = 2000
+    MATCH_SPEED_REASONABLY_FAST = 1999
 
     def match_drivers_and_pa(self, passengers, drivers):
         match_quality = Matcher.MATCH_QUALITY_WAY_HIGHER
-        match_speed =  Matcher.MATCH_SPEED_CRAZY_FAST
+        match_speed =  Matcher.MATCH_SPEED_REASONABLY_FAST
         for passenger in passengers:
             ...
 
