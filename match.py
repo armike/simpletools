a58fad83873957b0caa6258e64474d2d5f11495a
Mike Ross <armike@gmail.com>
2015-09-22 04:08:27 -0700

    Update match.py

--- match.py
+++ match.py
@@ -9,8 +9,8 @@ class Matcher(object):
     MATCH_SPEED_FAST = 10
 
     def match_drivers_and_pa(self, passengers, drivers):
-        match_quality = Match.MATCH_QUALITY_HIGH
-        match_speed =  Match.MATCH_SPEED_FAST
+        match_quality = Matcher.MATCH_QUALITY_HIGH
+        match_speed =  Matcher.MATCH_SPEED_FAST
         for passenger in passengers:
             ...
 
