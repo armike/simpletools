6c62a0c533cc4a8b7cf12362008ee2ca5b41e6a2
Mike Ross <mross@lyft.com>
2015-09-22 03:57:12 -0700

    dialed down the speed

--- match.py
+++ match.py
@@ -8,11 +8,11 @@ class Matcher(object):
     MATCH_QUALITY_HIGH = 100
     MATCH_QUALITY_WAY_HIGHER = 1000
     MATCH_SPEED_FAST = 10
-    MATCH_SPEED_CRAZY_FAST = 200
+    MATCH_SPEED_REASONABLY_FAST = 199
 
     def match_drivers_and_pa(self, passengers, drivers):
         match_quality = Match.MATCH_QUALITY_WAY_HIGHER
-        match_speed =  Match.MATCH_SPEED_CRAZY_FAST
+        match_speed =  Match.MATCH_SPEED_REASONABLY_FAST
         self.match_drivers_with_params
 
     def find_matches(self):
