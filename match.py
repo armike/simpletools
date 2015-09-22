618962b1caffb1018e169c6f3f03b926a55131da
Mike Ross <armike@gmail.com>
2015-09-22 04:09:39 -0700

    Update match.py

--- match.py
+++ match.py
@@ -6,11 +6,13 @@ Matching algorithms
 class Matcher(object):
 
     MATCH_QUALITY_HIGH = 100
+    MATCH_QUALITY_WAY_HIGHER = 200
     MATCH_SPEED_FAST = 10
+    MATCH_SPEED_CRAZY_FAST = 2000
 
     def match_drivers_and_pa(self, passengers, drivers):
-        match_quality = Matcher.MATCH_QUALITY_HIGH
-        match_speed =  Matcher.MATCH_SPEED_FAST
+        match_quality = Matcher.MATCH_QUALITY_WAY_HIGHER
+        match_speed =  Matcher.MATCH_SPEED_CRAZY_FAST
         for passenger in passengers:
             ...
 
